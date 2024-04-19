3ee0a8646d231e48609af9a8fa3e8d23bdf0657c
Tony Sharples <tonysharples22@me.com>
2024-04-19 16:20:41 +0100

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Tony Sharples on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
