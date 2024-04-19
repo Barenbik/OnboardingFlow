d4932f024a55125329729e3966a95cfa847e3ed5
Tony Sharples <tonysharples22@me.com>
2024-04-19 19:14:18 +0100

    Add an image and remove borders

--- OnboardingFlow/WelcomePage.swift
+++ OnboardingFlow/WelcomePage.swift
@@ -10,25 +10,28 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 150, height: 150)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 150, height: 150)
                 .foregroundStyle(.tint)
+                
+                Image(systemName: "figure.strengthtraining.traditional")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.white)
+            }
             
             Text("Welcome to MyApp")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .fontDesign(.rounded)
-                .border(.black, width: 1.5)
+                .padding(.top)
             
             Text("This is description text. I'm writing a bunch of text here so that it wraps across multiple lines.")
                 .font(.title2)
                 .fontDesign(.rounded)
                 .multilineTextAlignment(.center)
-                .border(.black, width: 1.5)
         }
-        .border(.yellow, width: 1.5)
         .padding()
-        .border(.purple, width: 1.5)
     }
 }
 
